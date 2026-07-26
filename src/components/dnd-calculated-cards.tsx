baff6e2e0ba3305cd44ce0f1f2f6590a3bb4b89a
George Tsomlektsis <ultrawelfaredev@gmail.com>
2026-07-26 13:33:33 +0300

    style: add top and bottom margin to calculated cards container

--- src/components/dnd-calculated-cards.tsx
+++ src/components/dnd-calculated-cards.tsx
@@ -14,7 +14,7 @@ interface DndCalculatedCardsProps {
 
 export default function DndCalculatedCards({cards, cardsPerRow}: DndCalculatedCardsProps) {
 	return (
-		<div className="grid gap-4" style={{gridTemplateColumns: `repeat(${cardsPerRow}, minmax(0, 1fr))`}}>
+		<div className="my-3 grid gap-4" style={{gridTemplateColumns: `repeat(${cardsPerRow}, minmax(0, 1fr))`}}>
 			{cards.map((c) => (
 				<Card className="w-full" key={c.label} label={c.label} value={c.value} sublabel={c.sublabel} offlabel={c.offlabel} />
 			))}
